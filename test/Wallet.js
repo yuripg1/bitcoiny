919f98f8cb1018787505064dee5e9556a6772cfc
Yuri Pelz Gossmann <yuripg1@gmail.com>
2021-02-21 15:09:08 -0300

    TypeScript init

--- test/Wallet.js
+++
@@ -1,7 +0,0 @@
-const Wallet = require('../lib/Wallet');
-
-describe('[Wallet] Create wallet from seed', () => {
-  it('Create wallet from 128 bit seed', () => {
-    Wallet.fromSeed(Buffer.from('1d64815716b8606db0bc27171099d250', 'hex'));
-  });
-});
